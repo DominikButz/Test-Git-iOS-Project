315b9b499037b47f87b3baadfc555dbed73675d0
Dominik Butz <Dominik@Dominiks-MacBook.local>
2013-10-16 20:17:51 +0800

    added a test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Dominik Butz on 16.10.13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
